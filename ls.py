e0c919bacf39f6ebfd9cc3d43026872e00f4164f
YHM07 <yangheming55@126.com>
2015-08-29 16:36:50 +0800

    添加了一些注释

--- ls.py
+++ ls.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+编写一个程序，能在当前目录以及当前目录的所有子目录下查找文件名包含指定字符串的文件，并打印出相对路径。
+"""
 __author__ = 'H.M.Yang'
 
 import os
@@ -19,11 +22,11 @@ def getfile(path, ext):
     for f in os.listdir(pwd):
         if os.path.isdir(os.path.join(pwd,f)):
             # pdb.set_trace()
-            # os.chdir(f)
+            os.chdir(f)
             # y = os.path.join(path,f)
-            # getfile('.',ext)
-            getfile(os.path.join(pwd,f),ext)
-            # os.chdir(pwd)
+            getfile(os.path.join(os.path.pardir,f),ext)
+            # getfile(os.path.join(pwd,f),ext)
+            os.chdir(os.path.pardir)
         if os.path.isfile(os.path.join(pwd,f)) and os.path.splitext(os.path.join(pwd,f))[1] == ext:
                 print(os.path.join(path, f))
                 
